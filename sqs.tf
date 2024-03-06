0c10d767cf02b94c465a0dde10c0693e9ee941b4
Misael Burboa <mburboa@consumertrack.com>
2024-03-06 13:23:25 -0700

    remove comments

--- sqs.tf
+++ sqs.tf
@@ -3,8 +3,4 @@ resource "aws_sqs_queue" "cvs-3797-example-queue" {
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
-  # redrive_policy = jsonencode({
-  #   deadLetterTargetArn = aws_sqs_queue.cvs-3797-example-queue_deadletter.arn
-  #   maxReceiveCount     = 4
-  # })
 }
