44e5b9d544e9281a558d3a3d923a0ced5e94ee45
Misael Burboa <mburboa@consumertrack.com>
2024-03-06 13:22:53 -0700

    remove unused event source

--- sqs-event-source.tf
+++
@@ -1,4 +0,0 @@
-# resource "aws_lambda_event_source_mapping" "sqs-event-source" {
-#   event_source_arn = aws_sqs_queue.cvs-3797-example-queue.arn
-#   function_name    = aws_lambda_function.cvs-3797-lambda.arn
-# }
