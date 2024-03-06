3db48c54cc490f0043e548298cf95b152bdfc101
Misael Burboa <mburboa@consumertrack.com>
2024-03-06 13:51:00 -0700

    Upgrade provider and renaming lambdas

--- lamda.tf
+++ lamda.tf
@@ -24,15 +24,15 @@ data "archive_file" "lambda" {
 }
 
 
-resource "aws_lambda_function" "cvs-3797-lambda" {
+resource "aws_lambda_function" "sqs-message-handler" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   filename      = "lambda_function_payload.zip"
-  function_name = "cvs-3797"
+  function_name = "sqs-message-handler"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.handler"
   source_code_hash = data.archive_file.lambda.output_base64sha256
-  runtime = "nodejs18.x"
+  runtime = "nodejs20.x"
 
   environment {
     variables = {
@@ -50,6 +50,6 @@ resource "aws_iam_role_policy_attachment" "lambda_sqs_role_policy" {
 
 
 resource "aws_lambda_event_source_mapping" "sqs-event-source" {
-  event_source_arn = aws_sqs_queue.cvs-3797-example-queue.arn
-  function_name    = aws_lambda_function.cvs-3797-lambda.arn
+  event_source_arn = aws_sqs_queue.test-messages-queue.arn
+  function_name    = aws_lambda_function.sqs-message-handler.arn
 }
--- sqs.tf
+++ sqs.tf
@@ -1,5 +1,5 @@
-resource "aws_sqs_queue" "cvs-3797-example-queue" {
-  name                      = "cvs-3797-example-queue"
+resource "aws_sqs_queue" "test-messages-queue" {
+  name                      = "test-messages-queue"
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
